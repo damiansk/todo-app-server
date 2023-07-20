6eab53b99473eba75c22294fcdd4816213b73aa8
dstolare <dstolarek@pgs-soft.com>
2023-07-20 07:38:13 +0200

    feat: dummy db initialization

--- scripts/server-init.sh
+++ scripts/server-init.sh
@@ -4,7 +4,11 @@ apt-get update
 
 apt-get install curl
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+
 apt-get install nodejs
+apt-get install mysql-client
+
+mysql -h dstolarek-todo-database-private.cgf1hknohtf8.us-east-1.rds.amazonaws.com -u admin -p *yxR4Yi1NiiXHt4W < ./Tasks.sql
 
 npm ci
 npm start
