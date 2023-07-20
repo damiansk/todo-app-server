f68cf90595f83833b1033773f534b9b978597744
dstolare <dstolarek@pgs-soft.com>
2023-07-20 07:49:36 +0200

    feat: pass default yes answer to install commands

--- scripts/server-init.sh
+++ scripts/server-init.sh
@@ -5,8 +5,8 @@ apt-get update
 apt-get install curl
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 
-apt-get install nodejs
-apt-get install mysql-client
+apt-get install nodejs -y
+apt-get install mysql-client -y
 
 mysql -h dstolarek-todo-database-private.cgf1hknohtf8.us-east-1.rds.amazonaws.com -u admin -p *yxR4Yi1NiiXHt4W < ./scripts/Tasks.sql
 
