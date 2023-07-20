307359898fb55179bb842da6cb228e9a0cbed7a1
dstolare <dstolarek@pgs-soft.com>
2023-07-20 07:58:26 +0200

    feat: run server as daemon

--- scripts/server-init.sh
+++ scripts/server-init.sh
@@ -10,5 +10,9 @@ apt-get install mysql-client -y
 
 mysql -h dstolarek-todo-database-private.cgf1hknohtf8.us-east-1.rds.amazonaws.com -u admin -p*yxR4Yi1NiiXHt4W < ./scripts/Tasks.sql
 
+npm install pm2 -g && pm2 update
+pm2 --version
+
 npm ci
-npm start
+
+pm2 --name TODO_Server start npm -- start
