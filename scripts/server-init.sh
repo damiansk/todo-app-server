85d186a5f20dcbab19267a728f7d2f6732a6c3ad
dstolare <dstolarek@pgs-soft.com>
2023-07-20 07:48:39 +0200

    feat: update db initialization

--- scripts/server-init.sh
+++ scripts/server-init.sh
@@ -8,7 +8,7 @@ curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 apt-get install nodejs
 apt-get install mysql-client
 
-mysql -h dstolarek-todo-database-private.cgf1hknohtf8.us-east-1.rds.amazonaws.com -u admin -p *yxR4Yi1NiiXHt4W < ./Tasks.sql
+mysql -h dstolarek-todo-database-private.cgf1hknohtf8.us-east-1.rds.amazonaws.com -u admin -p *yxR4Yi1NiiXHt4W < ./scripts/Tasks.sql
 
 npm ci
 npm start
